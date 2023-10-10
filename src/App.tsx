3bc853eb2b3a53bed9bbf467101ce4dad62b7b1d
Bartosz Wiecheć <32521016+bwiechec@users.noreply.github.com>
2023-10-10 21:33:32 +0200

    v 0.81

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,33 @@
-import './App.css';
-import Navbar from './components/Navbar'
-import Content from './components/Content'
-import LoginPage from './components/LoginPage'
-import QuizCreate from './components/quizCreate/QuizCreate'
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import LoginPage from "./components/LoginPage";
+import QuizCreate from "./components/quizCreate/QuizCreate";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import QuizList from "./components/quizList/QuizList";
 import Quiz from "./components/quiz/Quiz";
+import QuizCategories from "./components/quizCategories/QuizCategories";
 
 function App() {
-
   return (
     <div className="App">
-
       <BrowserRouter>
         <Navbar />
-        <Routes key={'Routes'}>
-          <Route key={'main_route'} path="/" element={<Content />} />
-          <Route key={'category'} path="/category/:categoryId" element={<QuizList />} />
-          <Route key={'login_rote'} path="/login" element={<LoginPage />} />
-          <Route key={'quiz_create_route'} path="/quiz/create" element={<QuizCreate />} />
-          <Route key={'quiz'} path="/quiz/:quizId" element={<Quiz />} />
+        <Routes key={"Routes"}>
+          <Route key={"main_route"} path="/" element={<QuizCategories />} />
+          <Route
+            key={"category"}
+            path="/category/:categoryId"
+            element={<QuizList />}
+          />
+          <Route key={"login_rote"} path="/login" element={<LoginPage />} />
+          <Route
+            key={"quiz_create_route"}
+            path="/quiz/create"
+            element={<QuizCreate />}
+          />
+          <Route key={"quiz"} path="/quiz/:quizId" element={<Quiz />} />
         </Routes>
       </BrowserRouter>
-
     </div>
   );
 }
